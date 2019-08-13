9b752a571d24d3350fbe4b4bf95347b9b6138650
Michael Bonner <michael@redolive.com>
2019-08-12 21:46:19 -0600

    initial commit

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Blog;
+use Faker\Generator as Faker;
+
+$factory->define(Blog::class, function (Faker $faker) {
+    return [
+        'name' => $faker->company,
+        'feed_url' => $faker->url
+    ];
+});
