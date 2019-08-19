c39ca42391062d58d9e8ca4cb57b56b41eb0e52f
Michael Bonner <michaelwbonner@gmail.com>
2019-08-19 12:05:50 -0600

    report verified after onboarding text

--- app/Console/Commands/VerifySubscription.php
+++ app/Console/Commands/VerifySubscription.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\BlogUser;
 use App\Services\Twilio;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 class VerifySubscription extends Command
@@ -44,6 +45,8 @@ class VerifySubscription extends Command
                 $blogUser->notify_location,
                 "Hey! It's your brother Mike. You'll be getting a text when the parents add a new post to their blog. If you don't want this, just reply stop."
             );
+            $blogUser->verified_at = Carbon::now();
+            $blogUser->save();
         });
     }
 }
--- app/Models/BlogUser.php
+++ app/Models/BlogUser.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class BlogUser extends Pivot
 {
+    public $timestamps = false;
+
     public function user()
     {
         return $this->belongsTo(User::class);
